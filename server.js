28cdfcffc02b5a32a77b197375a8f18755ba1a2a
Hamish Isaacs <hamisaacs@gmail.com>
2021-02-07 18:47:34 +0800

    removed api-routes and html-routes.js. Add README

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-// import dempendencies to interact with front end
-const express = require('express');
-// need filename path
-const path = require('path');
-// require fs to read and write to files
-const fs = require('fs');
+// importing dependencies to interact with the front end
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-// this will create and express server
+// creating a server
 const app = express();
 
-// setting the initial PORT listner
-const PORT = 3000
+// Setting a port listener
+const PORT = 3000;
 
-// creating an array for notes data
- const noteData = [];
+// HTML GET Requests
 
+// when the Get started button is clicked display the note.html Web page 
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
-// setting express middleware to handle data parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static('./assets'));
+// If no matching route is found, then default to home
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-// here we set up routes
+app.get("/api/notes", function (req, res) {
+  return res.sendFile(path.json(__dirname, "db/db.json"));
+});
 
-
-// Listening
-
-app.listen(PORT, function() {
-    console.log(`Listneing on PORT ${ PORT }`);
+// Start the server on the port
+app.listen(PORT, function () {
+  console.log("SERVER IS LISTENING: " + PORT);
 });
