f30a9e4cdf0e653c1623ed52964ec69f9844ea68
Hamish Isaacs <hamisaacs@gmail.com>
2021-02-08 15:48:14 +0800

    Added api call response for the notes using array

--- server.js
+++ server.js
@@ -9,9 +9,76 @@ const app = express();
 // Setting a port listener
 const PORT = 3000;
 
+//  createNoteData Array
+let createNoteData = [];
+
+// Setting up middleware body parsing, static, and route
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+// api call response for the notes, an having results sent to browser in the form of an array of object
+app.get("/api/notes", function (err, res) {
+  try {
+    createNoteData = fs.readFileSync("db/db.json", "utf8");
+    console.log("Hello from the SERVER!");
+    createNoteData = JSON.parse(createNoteData);
+  } catch (err) {
+    console.log("\n error (catch err app.get):");
+    console.log(err);
+  }
+  res.json(createNoteData);
+});
+
+// this section writes the new note to the json file and sending back to the browser
+app.post("/api/notes", function (req, res) {
+  try {
+    createNoteData = fs.readFileSync("./db/db.json", "utf8");
+    console.log(createNoteData);
+    createNoteData = JSON.parse(createNoteData);
+    req.body.id = createNoteData.length;
+    createNoteData.push(req.body);
+    createNoteData = JSON.stringify(createNoteData);
+    fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) {
+      if (err) throw err;
+    });
+
+    res.json(JSON.parse(createNoteData));
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
+});
+
+// Deleting a note and reading the json file | writing the new notes to the file and sending back to the browser
+
+app.delete("/api/notes/:id", function (req, res) {
+  try {
+    createNoteData = fs.readFileSync("./db/db.json", "utf8");
+    createNoteData = JSON.parse(createNoteData);
+    createNoteData = createNoteData.filter(function (note) {
+      return note.id != req.params.id;
+    });
+    createNoteData = JSON.stringify(createNoteData);
+
+    fs.writeFile("./db/db.json", createNoteData, "utf8", function (err) {
+      if (err) throw err;
+    });
+
+    res.send(JSON.parse(createNoteData));
+  } catch (err) {
+    throw err;
+    console.log(err);
+  }
+});
+
 // HTML GET Requests
 
-// when the Get started button is clicked display the note.html Web page 
+// when the Get started button is clicked display the note.html Web page
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
